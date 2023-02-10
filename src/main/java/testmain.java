e5783f0f5e852533f7da3bb202e871e9da4a0817
CorglD <52209782@edu.campus02.at>
2023-02-10 13:34:20 +0100

    Initial commit

---
+++ src/main/java/testmain.java
@@ -0,0 +1,19 @@
+public class testmain {
+
+    public int getZahl() {
+        return zahl;
+    }
+
+    public void setZahl(int zahl) {
+        this.zahl = zahl;
+    }
+
+    private int zahl;
+
+
+    public testmain(int zahl) {
+        this.zahl = zahl;
+    }
+
+
+}
---
+++ src/test/java/testmainTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class testmainTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getZahl() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setZahl() {
+    }
+}
